e9040bc75e17676dea5cca6e5e0516257aee3b5b
Joan Gabriel Peralta Santana <joanperalta13@gmail.com>
2017-12-15 23:28:10 -0400

    Define some pending tests

---
+++ test/integration/problems.js
@@ -0,0 +1,8 @@
+import test from 'ava'
+
+test.todo('problem topic - throws an error if there is missing or invalid parameters creating new one')
+test.todo('problem topic - can create a new one')
+test.todo('problem topic - can aprove a new one pending of moderation')
+test.todo('problem topic - can vote positive/negative')
+test.todo('problem topic - can get a top ranking of problem topics with most positive votes')
+test.todo('problem topic - can disable/remove a problem topic')
---
+++ test/integration/users.js
@@ -0,0 +1,8 @@
+import test from 'ava'
+
+test.todo('users - throws an error if there is missing or invalid parameters creating new one')
+test.todo('users - can create a new one')
+test.todo('users - can edit basic profile information')
+test.todo('users - can change password')
+test.todo('users - can perform login and generate a session token')
+test.todo('users - can validate a pending of validation account')
